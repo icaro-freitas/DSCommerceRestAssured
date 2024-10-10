b2681b34ebdd385cc06bfb01d8c32b9be80f81ea
icaro-freitas <icaro_freitas2014@outlook.com>
2024-10-09 21:36:46 -0300

    Test for findAll with name filter added

--- src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -1,12 +1,14 @@
 package com.devsuperior.dscommerce.controllers;
 
 
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
 
 public class ProductControllerRA {
 	
@@ -14,9 +16,13 @@ public class ProductControllerRA {
 	
 	private Long nonExistingProductId;
 	
+	private String productName;
+	
 	@BeforeEach
 	public void setUp() {
 		baseURI = "http://localhost:8080";
+		
+		productName = "Macbook";
 	}
 	
 	@Test
@@ -32,8 +38,37 @@ public class ProductControllerRA {
 		.body("imgUrl", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"))
 		.body("price", is(2190.0F))
 		.body("categories.id", hasItems(2,3))
-		.body("categories.name", hasItems("Eletrônicos","Computadores"));		
-		
+		.body("categories.name", hasItems("Eletrônicos","Computadores"));	
+	}
+	
+	@Test
+	public void findAllShouldReturnPageProductsWhenProductNameIsEmpty() {
+		given()
+		.get("/products?page=0")
+		.then()
+		.statusCode(200)
+		.body("content.name", hasItems("Macbook Pro","PC Gamer Tera"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPageProductsWhenProductNameIsNotEmpty() {
+		given()
+		.get("/products?name={productName}", productName)
+		.then()
+		.statusCode(200)
+		.body("content.id[0]", is(3))
+		.body("content.name[0]", equalTo("Macbook Pro"))
+		.body("content.price[0]", is(1250.0F))
+		.body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPageProductsWithPriceGreaterThan2000() {
+		given()
+		.get("/products?size=25")
+		.then()
+		.statusCode(200)
+		.body("content.findAll { it.price > 2000 }.name", hasItems("Smart TV", "PC Gamer Weed"));
 	}
 
 }
